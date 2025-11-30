bf610a1691892054db2222f8c10c2689b53718d8
Lateefah camacho <lateefah.camacho07@gmail.com>
2025-11-30 12:10:44 -0800

    modify class

--- IntBinaryTree.h
+++ IntBinaryTree.h
@@ -2,12 +2,13 @@
 #ifndef INTBINARYTREE_H
 #define INTBINARYTREE_H
 
+
 // The IntBinaryTree class manages a binary tree of integers.
-class IntBinaryTree {
+class StringBinaryTree {
 private:
    // TreeNode is a private structure for IntBinaryTree nodes.
    struct TreeNode {
-      int value;         // The value in the node
+      string value;         // changed from int to string
       TreeNode *left;    // Pointer to left child node
       TreeNode *right;   // Pointer to right child node
    };
@@ -18,7 +19,7 @@ private:
    // Private member functions for internal operations.
    void insert(TreeNode *&, TreeNode *&);
    void destroySubTree(TreeNode *);
-   void deleteNode(int, TreeNode *&);
+   void deleteNode(string, TreeNode *&); // changed to string
    void makeDeletion(TreeNode *&);
    void displayInOrder(TreeNode *) const;
    void displayPreOrder(TreeNode *) const;
@@ -26,15 +27,15 @@ private:
 
 public:
    // Constructor initializes the root to nullptr, indicating an empty tree.
-   IntBinaryTree()     { root = nullptr; }
+   StringBinaryTree()     { root = nullptr; } // same now for strings
 
    // Destructor deallocates all nodes in the tree.
-   ~IntBinaryTree()    { destroySubTree(root); }
+   ~StringBinaryTree()    { destroySubTree(root); }
 
-   // Public interface for inserting, searching, and removing nodes.
-   void insertNode(int);
-   bool searchNode(int);
-   void remove(int);
+   // Public interface for inserting, searching, and removing nodes (changed from int to string)
+   void insertNode(string);
+   bool searchNode(string);
+   void remove(string);
 
    // Public wrappers for tree traversal functions.
    void displayInOrder() const     {  displayInOrder(root); }
